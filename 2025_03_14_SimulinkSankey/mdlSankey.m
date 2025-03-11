897ecccde317a84555cb557811ed17beb0513614
grouleau <grouleau@VDI-AH2WDPG-008>
2025-03-11 16:26:31 -0400

    Added sankey post

---
+++ 2025_03_14_SimulinkSankey/mdlSankey.m
@@ -0,0 +1,19 @@
+function SC = mdlSankey(mdl,lim)
+    arguments
+        mdl char
+        lim (1,1) {mustBeNumeric} = 1;
+    end
+    warning('off','SankeyChart:UnbalancedGraph');
+    % Fill the table with find_system
+    T = mdlTable(table(),mdl);
+    % Cut elements with less blocks than "lim"
+    T(T.Weight<lim,:) = [];
+    % create digraph
+    DG = digraph(T);
+    % Plot
+    f = uifigure;
+    SC = SankeyChart( "Parent", f, "GraphData", DG );
+    SC.LabelIncludeTotal = "on";
+    % Fix labels
+    SC = fixBlkNames(SC);
+end
